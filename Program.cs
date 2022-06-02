0920ae6ec5377cea7d15e70e1b0080eeaa2e5904
KadenCodes <106559706+KadenCodes@users.noreply.github.com>
2022-06-02 15:53:07 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace AmIOnline
+{
+    public partial class AmIOnline
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("What is the ip?");
+            string ip = Console.ReadLine();
+
+            //pinger
+
+            Ping myPing = new Ping();
+            PingReply reply = myPing.Send(ip, 1000);
+            Console.WriteLine("Status : " + reply.Status);
+            Console.WriteLine("T.O.P ===> " + reply.RoundtripTime.ToString());
+            Console.WriteLine("Sent To : " + reply.Address);
+        }
+    }
+}
